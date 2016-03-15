422e25761613a5657437d4864737925080e9c5f3
U-JWORD\nakagawa <nakagawa@PC15011901.jword.local>
2016-03-15 14:22:29 +0900

    edit test

--- SampleTest.php
+++ SampleTest.php
@@ -14,6 +14,6 @@ class SampleTest extends PHPUnit_Framework_TestCase
     public function aaa()
     {
         $Sample = new Sample();
-        $this->assertEquals($Sample->helloWorld(), 'aaaaa');
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
 }
